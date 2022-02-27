67f85c9f8ff0f83ef75893c6f3c852488cd5a128
mdtouhidulislam136 <mdtouhidul.islam136@gmail.com>
2022-02-27 15:17:23 +0200

    true false testing

---
+++ true-false.js
@@ -0,0 +1,16 @@
+/* 
+true
+any number (positive or negative), any string including single whitespace
+
+false
+ number 0, empty string
+*/
+
+const x = {};
+if (x){
+    console.log('variable is true');
+}
+
+else{
+    console.log('variable false');
+}
